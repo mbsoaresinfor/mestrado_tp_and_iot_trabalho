3f105d164e8b5bab4ae708a8f94b894c1dfb05f0
Marcelo Barbosa Soares <marcelo.soares@cwi.com.br>
2021-03-26 14:43:47 -0300

    first commit

---
+++ src/test/java/com/mbs/dw/DesenWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbs.dw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesenWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
